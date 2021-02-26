3d682ef5af960e348474807b2a283f032f769d5e
chendapeng <dapengchenxjtu@foxmail.com>
2021-02-26 14:18:20 +0800

    preprare for string decompostion

--- gnStack.h
+++ gnStack.h
@@ -4,48 +4,48 @@
 
 #ifndef AL_LIST_GNSTACK_H
 #define AL_LIST_GNSTACK_H
-
 #include <vector>
 
-template<class T, int capacity=10>
-class Stack{
-public:
-    Stack(){
-        data.reserve(capacity);
-    }
+namespace Parrots {
+    template<class T, int capacity = 10>
+    class Stack {
+    public:
+        Stack() {
+            data.reserve(capacity);
+        }
 
-    void clear(){
-        data.clear();
-    }
+        void clear() {
+            data.clear();
+        }
 
-    bool isEmpty() const{
-        return data.empty();
-    }
+        bool isEmpty() const {
+            return data.empty();
+        }
 
-    // pop && push
-    // 1, vector.back(): back 是取值的意思
-    // 2, vector.pop_back(): 是把最后一个元素去除
+        // pop && push
+        // 1, vector.back(): back 是取值的意思
+        // 2, vector.pop_back(): 是把最后一个元素去除
 
-    T& E1_val(){
-        return data.back();
-    }
+        T &E1_val() {
+            return data.back();
+        }
 
-    T pop(){
-        T e1 = data.back();
-        data.pop_back();
-        return e1;
-    }
+        T pop() {
+            T e1 = data.back();
+            data.pop_back();
+            return e1;
+        }
 
-    void push(const T& e1){
-        data.push_back(e1);
-    }
+        void push(const T &e1) {
+            data.push_back(e1);
+        }
 
-private:
-    std::vector<T> data;
-
-
-};
+    private:
+        std::vector<T> data;
 
 
+    };
+
+}
 
 #endif //AL_LIST_GNSTACK_H
---
+++ stack_addlargenumbers.cpp
@@ -0,0 +1,13 @@
+//
+// Created by hp on 26/02/2021.
+//
+#include "gnStack.h"
+#include <string>
+
+
+std::string add_largenumbers(const std::string& n1, const std::string&n2) {
+    return "124";
+}
+
+
+
