679191949a8c2b801ff5c12fcee93ef929a0dc0b
chendapeng <dapengchenxjtu@foxmail.com>
2021-02-26 15:01:10 +0800

    add md learning notes on how to remove specific characters.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <array>
 #include <string>
-
+#include <algorithm>
 
 
 int main() {
 
-
+    std::string note1="1234 367 1234";
+    auto removed_end =std::remove(note1.begin(), note1.end(),' ');
+    note1.erase(removed_end, note1.end());
+    std::cout<<note1<<std::endl;
 
 
 
