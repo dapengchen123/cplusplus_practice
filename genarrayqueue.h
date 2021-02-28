d8a92427fdfb68c862a84fb24852607306510eee
chendapeng <dapengchenxjtu@foxmail.com>
2021-02-28 15:35:21 +0800

    add the reverse description in the note.

--- genarrayqueue.h
+++ genarrayqueue.h
@@ -5,26 +5,58 @@
 #ifndef AL_LIST_GENARRAYQUEUE_H
 #define AL_LIST_GENARRAYQUEUE_H
 
-template<class T, int size = 100>
-class ArrayQueue {
-public:
-    ArrayQueue(){
-        first = last =-1;
+namespace Parrots {
+    template<class T, int size = 100>
+    class ArrayQueue {
+    public:
+        ArrayQueue() {
+            first = last = -1;
+        }
+
+        void enqueue(T);
+
+        T dequeue();
+
+        bool isFull() {
+            return (first == 0 && last == size - 1) || last + 1 == first;
+        }
+
+        bool isEmpty() {
+            return first == -1;
+        }
+
+    private:
+        int first, last;
+        T storage[size]
+    };
+
+    template<class T, int size>
+    void ArrayQueue<T, size>::enqueue(T el) {
+        if (!isFull()) {
+            if (last == size - 1 || last == -1) {
+                storage[0] = el;
+                last = 0;
+                if (first == -1)
+                    first = 0;
+            } else {
+                storage[++last] = el;
+            }
+
+        } else std::cout << "Full queue. \n";
     }
 
-    void enqueue(T);
-    T dequeue();
-    bool isFull(){
-        return (first==0 && last==size-1) || last+1==first;
-    }
-    bool isEmpty(){
-        return first == -1;
+    template<class T, int size>
+    T ArrayQueue<T, size>::dequeue() {
+        if (!isEmpty()) {
+            T tmp = storage[first];
+            if(first==last) first=last=-1;
+            elseif (first==size-1)   first =0;
+            else first = first+1;
+            return tmp;
+        }
+        else std::cout << "Empty queue. \n";
     }
 
-private:
-    int first, last;
-    T storage[size]
-};
-
+}
 
 #endif //AL_LIST_GENARRAYQUEUE_H
